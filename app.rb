e70c637992f75b55f2b2b535d85f5eab8db42ba2
stephen ajay & debbie balbo <student@epicodus.com>
2015-06-15 16:08:40 -0700

    all is functioning

--- app.rb
+++ app.rb
@@ -18,3 +18,34 @@ post('/lists') do
   @list = List.create(:name => name)
   redirect back
 end
+
+
+get('/lists/:id') do
+  @list = List.find(params.fetch('id').to_i)
+  erb(:list)
+end
+
+
+post('/tasks') do
+  description = params.fetch('description')
+  list_id = params.fetch('list_id')
+  @task = Task.create({:description => description, :list_id => list_id})
+  @list = List.find(params.fetch('list_id').to_i)
+  erb(:list)
+end
+
+
+get('/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i)
+  erb(:task_edit)
+end
+
+patch('/edit/:id') do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i)
+  @task.update({:description => description})
+
+  @list = List.find(@task.list())
+
+  erb(:list)
+end
