e25800be949313be20fb84ac1bcb4c2d3ddb4568
stephen ajay & debbie balbo <student@epicodus.com>
2015-06-15 16:17:27 -0700

    added validation for description of tasks

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -10,6 +10,11 @@ describe(Task) do
         expect(task.list).to(eq(list))
       end
     end
+
+      it('validates the presence of description') do
+        task = Task.new({:description => ''})
+        expect(task.save).to(eq(false))
+    end
   describe('.not_done') do
     it('returns the not done tasks') do
       not_done_task1 = Task.create({:description => 'get it done', :done => false})
